92cc8fcd4ddf546582d99ea6582787fa1b8f25f5
Sidebail <tfg.devteam@gmail.com>
2018-12-04 13:01:18 -0500

    Simple fix thanks to Rich

--- API_Assignment2/Models/Review.cs
+++ API_Assignment2/Models/Review.cs
@@ -7,7 +7,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API_Assignment2.Models
 {
-    [Table("Review")]
+    [Table("reviews")]
     public class Review
     {
         [Key]
